d46cc64575474ea10e53a7e11ef09115bb5e97cb
Saumya Mistry <mistryasaumya@gmail.com>
2022-02-02 18:37:48 -0500

    Updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
+mongoose.connect('mongodb+srv://Saumya1309:Saumya555@comp3123.krhpr.mongodb.net/sample_mflix?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
